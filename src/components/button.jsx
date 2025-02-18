87a19c25a29f3c5b0c4508749a54e5ee068e390f
iiuperboyii <aaronjomon24@gmail.com>
2025-02-18 09:46:31 +0530

    completed required task

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,12 @@
 // write button card here
+import React from 'react'
+
+const Viewbutton = () => {
+  return (
+    <div>
+      <button className="w-full bg-blue-600 text-white font-semibold py-2 rounded-lg mt-4 hover:bg-blue-700 transition duration-300">View Products</button>
+    </div>
+  )
+}
+
+export default Viewbutton
